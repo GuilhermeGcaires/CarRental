cd3b42203e4206de0468dcb7736a476df2840af9
guilhermegcaires <guilhermegcaires@gmail.com>
2022-08-04 20:32:27 -0300

    Added sentry

--- src/shared/infra/app.ts
+++ src/shared/infra/app.ts
@@ -11,11 +11,29 @@ import { AppError } from "@shared/errors/AppError";
 import swaggerFile from "../../swagger.json";
 import { router } from "./http/routes";
 import upload from "@config/upload";
-
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter"
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 const app = express();
 
+app.use(rateLimiter)
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+
 app.use(express.json());
+
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
@@ -24,6 +42,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if(err instanceof AppError) {
